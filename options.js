b1cceffcf548092310271d105dee34261ee929ff
Steve Laing <steve.laing@gmail.com>
2019-06-21 18:06:41 +0100

    Refine options rendering and supply defaults

--- options.js
+++ options.js
@@ -1,18 +1,36 @@
+let githubOAuthTokenEl  = document.getElementById('github-oauth-token')
+let githubOrgNameEl     = document.getElementById('github-org-name')
+let concourseBaseUrlEl  = document.getElementById('concourse-base-url')
+let concourseTeamNameEl = document.getElementById('concourse-team-name')
+
+let loadOptions = () => {
+  chrome.storage.local.get([
+    'githubOAuthToken',
+    'githubOrgName',
+    'concourseBaseUrl',
+    'concourseTeamName'
+  ], data => {
+    githubOAuthTokenEl.value = data.githubOAuthToken || ''
+    githubOrgNameEl.value = data.githubOrgName || 'alphagov'
+    concourseBaseUrlEl.value = data.concourseBaseUrl || 'https://cd.gds-reliability.engineering'
+    concourseTeamNameEl.value = data.concourseTeamName || ''
+  })
+}
+
 let saveOptions = () => {
-  let githubOAuthToken  = document.getElementById('github-oauth-token').value
-  let githubOrgName     = document.getElementById('github-org-name').value
-  let concourseBaseUrl  = document.getElementById('concourse-base-url').value
-  let concourseTeamName = document.getElementById('concourse-team-name').value
+  let githubOAuthToken  = githubOAuthTokenEl.value
+  let githubOrgName     = githubOrgNameEl.value
+  let concourseBaseUrl  = concourseBaseUrlEl.value
+  let concourseTeamName = concourseTeamNameEl.value
 
   chrome.storage.local.set({
     githubOAuthToken : githubOAuthToken,
     githubOrgName : githubOrgName,
     concourseBaseUrl : concourseBaseUrl,
     concourseTeamName : concourseTeamName
-  }, () => {
-    let messageElement = document.getElementById('message')
-    setTimeout(() => { messageElement.textContent = 'Options saved' }, 750)
-  })
+  }, () => { setTimeout(() => { document.location.href = '/popup.html' }, 750) })
 }
 
 document.getElementById('save').addEventListener('click', saveOptions)
+
+loadOptions()
